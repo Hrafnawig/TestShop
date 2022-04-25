4b0571ad851865492af92f42eebfca4d61e6cfa1
Hrafnawig <kobhraf@gmail.com>
2022-04-25 17:42:12 +0300

    add dict

--- front/src/pages/HomeComponents/ShopCards.jsx
+++ front/src/pages/HomeComponents/ShopCards.jsx
@@ -3,6 +3,12 @@ import common from './common.module.scss';
 import shopCards from './shopCards.module.scss';
 import { useSelector, useDispatch } from "react-redux";
 import {getProducts} from "../../connection/shopSlice";
+import orange from '../../media/products/orange.jpeg';
+import apple from '../../media/products/apple.jpeg';
+import grapes from '../../media/products/grapes.jpeg';
+import nut from '../../media/products/nut.png';
+import potato from '../../media/products/potato.jpg';
+import watermelon from '../../media/products/watermelon.jpg';
 const ShopCards = () => {
     const dispatch = useDispatch();
     let productList = useSelector(state => state.shop.list)
@@ -12,12 +18,22 @@ const ShopCards = () => {
              dispatch(getProducts())
         }
     }, [postStatus, dispatch])
+    const picDictionary = {
+        orange,
+        apple,
+        nut,
+        watermelon,
+        grapes,
+        potato
+    }
     return (
         <div className={shopCards.background}>
             {productList.map((value) => {
                return value.data.map((value) => {
                     return <div className={common.center}>
-                        <div className={shopCards.cardDesign}>{value.price}</div>
+                        <div className={`${shopCards.cardDesign} ${common.centerColumn}`}>
+                            {value.price}
+                        </div>
                     </div>
                 })
             })}
